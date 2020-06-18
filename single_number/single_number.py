d5b5a924182b6eb7ffb8da340796b5b52f08568d
RichVI <richardwang2050@gmail.com>
2020-06-17 23:18:25 -0700

    MVP

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,13 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    
+    for x in arr:
+        if arr.count(x) == 1:
+            return x
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 9, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0, 11]
 
     print(f"The odd-number-out is {single_number(arr)}")
